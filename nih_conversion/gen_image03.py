1fc8230f4a0e69e854c9a62ea2986e94e99ae52a
Haley Acevedo <haley.acevedo2@gmail.com>
2019-12-20 10:34:13 -0600

    simplified process for grabbing scan zips

--- nih_conversion/gen_image03.py
+++ nih_conversion/gen_image03.py
@@ -5,16 +5,17 @@ import os
 import os.path
 import pandas as pd
 import pydicom
+import re
 import sys
-sys.path.append('/data/nil-bluearc/black/git/utils')
-
+sys.path.append('/net/zfs-black/BLACK/black/git/utils/4dfp')
 
 from getpass import getpass
-from instructions import get_first_dicom
+from instructions import find_dicoms
 
 
-study_dir = '/data/nil-bluearc/black/NewTics'
+study_dir = '/net/zfs-black/BLACK/black/NewTics'
 
+# Sequence info to include in CSV
 scan_type_map = {
     'mprs': {
         'type': 'MR structural (MPRAGE)',
@@ -42,48 +43,39 @@ scan_type_map = {
 
 columns = ['demo_study_id', 'interview_date', 'image_file', 'image_description', 'experiment_id', 'scan_type']
 
-
+"""
+Create base of image03 NIH submission file 
+    - Extracts information pertaining to each scan and stores in CSV alongside zip file path
+Note: needs to be run from unix since it relies on the sorted study files amd Windows seems unable to follow the symlinks
+"""
 def gen_image03():
-    script_dir = os.getcwd()
-
     os.chdir(study_dir)
     patids = [ d for d in glob.glob('NT*') if os.path.isdir(d) ]
 
+    # read in config that contains series description/scan type map (same file used for creating params file)
     with open('NT_config.json') as f:
         scan_mapping = json.load(f)['series_desc_mapping']
 
     results = []
     processed_zips = []
     for patid in patids:
-        if not os.path.exists(os.path.join(patid, 'scans.studies.txt')):
-            print('No txt for patid:', patid)
-            continue
+        scan_zips = glob.glob('zips/{}_study*.zip'.format(patid)) # get all zips for patid
 
         os.chdir(patid)
-
-
-
-        df = pd.read_table('scans.studies.txt', delim_whitespace=True, names=['id', 'scan_type', 'series_desc', 'file_count'])
-        for _, row in df.iterrows():
-            if row['series_desc'] not in scan_mapping:
-                continue
-
-            scan_info = scan_type_map[scan_mapping[row['series_desc']]]
-
-            scan_zips = [ f for f in glob.glob('../zips/{}*_study{}*.zip'.format(patid, row['id'])) if f not in processed_zips ]
-            for zip in scan_zips:
-                if not os.path.exists(zip):
-                    print('Cannot find zip:', zip)
-                    continue
-
-                #print(patid, row['id'])
-                scan_date = pydicom.dcmread(get_first_dicom(row['id'], True)).StudyDate
-                results.append([patid.split('_')[0], scan_date, zip, scan_info['desc'], scan_info['exp_id'] if 'exp_id' in scan_info else None, scan_info['type']])
-                processed_zips.append(zip)
-
+        for zip in scan_zips:
+            scan_num = re.search('study(\d+)', zip).group(1)
+            
+            try:
+                ds = pydicom.dcmread(find_dicoms(scan_num, True)[0]) # get pydicom dataset for a DICOM in the series
+                scan_info = scan_type_map[scan_mapping[ds.SeriesDescription]] # get scan_type_map key from scan_mapping config
+                results.append([patid.split('_')[0], ds.StudyDate, zip, scan_info['desc'], scan_info['exp_id'] if 'exp_id' in scan_info else None, scan_info['type']])
+            except IndexError:
+                print('Study folder missing for:', zip)
+            except FileNotFoundError:
+                print('Symlinks broken for:', zip)
+            except KeyError:
+                print('Series description not in map:', zip)
         os.chdir('..')
-
-    os.chdir(script_dir)
 
     image_df = pd.DataFrame(results, columns=columns).set_index('demo_study_id')
     image_df = image_df.assign(scan_object='Live', image_file_format='DICOM', image_modality='MRI', transformation_performed='No')
