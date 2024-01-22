59598f483b6bbeead5d8fdd2655811af715c7f93
Jonathan Koller <kollerj@wustl.edu>
2024-01-22 12:16:49 -0600

    Add more scan types to dcm_to_BIDS script

--- generic_dcm_to_BIDS.sh
+++ generic_dcm_to_BIDS.sh
@@ -24,15 +24,21 @@ dcm_sorted_dir=$(dirname "${params_file}")
 
 mprs=(`cat ${params_file} | grep "mprs" | cut -d"(" -f2 | cut -d")" -f1`)
 tse=(`cat ${params_file} | grep "tse" | cut -d"(" -f2 | cut -d")" -f1`)
+nmw=(`cat ${params_file} | grep "nmw" | cut -d"(" -f2 | cut -d")" -f1`)
+megre=(`cat ${params_file} | grep "megre" | cut -d"(" -f2 | cut -d")" -f1`)
 flair=(`cat ${params_file} | grep "flair" | cut -d"(" -f2 | cut -d")" -f1`)
+pdt2=(`cat ${params_file} | grep "pdt2" | cut -d"(" -f2 | cut -d")" -f1`)
 fstd=(`cat ${params_file} | grep "fstd" | cut -d"(" -f2 | cut -d")" -f1`)
+sbref=(`cat ${params_file} | grep "sbref" | cut -d"(" -f2 | cut -d")" -f1`)
 gre=(`cat ${params_file} | grep "gre" | cut -d"(" -f2 | cut -d")" -f1`)
+sefm=(`cat ${params_file} | grep "sefm" | cut -d"(" -f2 | cut -d")" -f1`)
 dwi=(`cat ${params_file} | grep "dwi" | cut -d"(" -f2 | cut -d")" -f1`)
 pcasl=(`cat ${params_file} | grep "pcasl" | grep -v "pcasl_m0" | cut -d"(" -f2 | cut -d")" -f1`)
 pcasl_m0=(`cat ${params_file} | grep "pcasl_m0" | cut -d"(" -f2 | cut -d")" -f1`)
-pcasl=(`cat ${params_file} | grep "pasl" | grep -v "pasl_m0" | cut -d"(" -f2 | cut -d")" -f1`)
+pasl=(`cat ${params_file} | grep "pasl" | grep -v "pasl_m0" | cut -d"(" -f2 | cut -d")" -f1`)
+pasl_m0=(`cat ${params_file} | grep "pasl_m0" | cut -d"(" -f2 | cut -d")" -f1`)
 struct_series=(${mprs[@]} ${tse[@]} ${flair[@]})
-other_series=(${fstd[@]} ${gre[@]} ${dwi[@]} ${pcasl[@]} ${pcasl_m0[@]} ${pasl[@]})
+other_series=(${fstd[@]} ${sbref[@]} ${gre[@]} ${sefm[@]} ${dwi[@]} ${pcasl[@]} ${pcasl_m0[@]} ${pasl[@]} ${pasl_m0[@]} ${megre[@]} ${pdt2[@]} ${nmw[@]})
 
 for dicom_series in ${struct_series[@]}
 do
@@ -44,6 +50,7 @@ do
 	dicom_dirs="${dicom_dirs}"" ${dcm_sorted_dir}/study${dicom_series}"
 done
 
+echo "DICOM DIRS = ${dicom_dirs}"
 
 heudiconv \
 	--outdir ${bids_dir} \
